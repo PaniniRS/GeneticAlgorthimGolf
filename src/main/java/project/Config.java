aa1771ceaf794754996a05fdfb0e79d93bfd78ec
PaniniRS <bubemanchester@live.com>
2025-01-08 16:52:11 +0100

    Tweaked parameters

--- src/main/java/project/Config.java
+++ src/main/java/project/Config.java
@@ -1,10 +1,10 @@
 package project;
 
 public class Config {
-    public static final int GENERATIONS = 10000;
-    public static final double HOLEPOS = 1000.0;
-    public static final int POPSIZE = 5000;
-    public static final int BEST_POP_TO_GET = 6;
+    public static final int GENERATIONS = 500_000;
+    public static final double HOLEPOS = 10000.0;
+    public static final int POPSIZE = 1000;
+    public static final int BEST_POP_TO_GET = 4;
     public static final double MUTATION_RATE = 0.1;
     public static final double CROSSOVER_RATE = 0.6;
 
@@ -15,6 +15,6 @@ public class Config {
     public static final int SEED = 1;
 
     public static final int ANGLE_BOUND = 180;
-    public static final int VELOCITY_BOUND = 2000;
+    public static final int VELOCITY_BOUND = 5000;
     public static final int POSX_INIT_BOUND = 1000;
 }
