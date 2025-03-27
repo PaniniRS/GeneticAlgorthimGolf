e55b579ce42b5cece49eced20e94bd65e92fa641
PaniniRS <bubemanchester@live.com>
2025-03-27 15:36:44 +0100

    Created new Runnable task for multithreaded

---
+++ src/main/java/project/MultiThreaded.java
@@ -0,0 +1,42 @@
+package project;
+
+import util.Helper;
+import util.LogLevel;
+import util.Logger;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+
+import static project.Config.*;
+
+public class MultiThreaded implements Runnable{
+    Random r;
+    ArrayList<Ball> population;
+    int indexStart, indexEnd;
+    MultiThreaded(Random r, int indexStart, int indexEnd, ArrayList<Ball> population){
+        this.r = r;
+        this.population = population;
+        this.indexStart = indexStart;
+        this.indexEnd = indexEnd;
+    }
+    @Override
+    public void run()  {
+//        Logger.log("Running MultiThreaded");
+        for (int i = indexStart; i < indexEnd; i++) {
+            //Fitness
+            Ball ball = population.get(i);
+//            Logger.log("\t-Init ball, fitness: " + ball.getFitness());
+            ball.setFitness(ball.evaluateFitness());
+//            Logger.log("\t--Eval Ball, fitness: " + ball.getFitness());
+        }//indexLoop
+        try {
+            Logger.log("Waiting for barrier");
+            BARRIER.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
